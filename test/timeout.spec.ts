21476c3d9d765224d42d40c4bea664ae80c7b85c
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-06-10 18:13:34 +0200

    test concatMap in interval counters

--- test/timeout.spec.ts
+++ test/timeout.spec.ts
@@ -1,7 +1,12 @@
-import { map, mergeMap, switchMap, take } from 'rxjs/operators';
+import { concatMap, map, mergeMap, switchMap, take } from 'rxjs/operators';
 import { interval } from 'rxjs';
 import clock = jasmine.clock;
-import { mergeMapResultCounter, MessageTimeout, switchMapResultCounter } from './mocks/async-data';
+import {
+  concatMapResultCounter,
+  mergeMapResultCounter,
+  MessageTimeout,
+  switchMapResultCounter
+} from './mocks/async-data';
 
 describe('test timeouts with numeric streams', () => {
   let testResultLastStream = '';
@@ -23,7 +28,7 @@ describe('test timeouts with numeric streams', () => {
   });
 
   // se resolverán todos los observables, pero en sin respetar el orden de llegada (el quien llega antes se resuelve)
-  it('combine stream with merge map', () => {
+  it('combine stream with mergeMap', () => {
     longStream.pipe(
       mergeMap((long: number) => shortStream.pipe(map((short: number) => `${long} : ${short}`)))
     ).subscribe((resultMap: string) => {
@@ -35,8 +40,8 @@ describe('test timeouts with numeric streams', () => {
     expect(testResultLastStream.trim()).toEqual(mergeMapResultCounter.trim());
   });
 
-  // de cada iteracion solo se resolvera si el ciclo corto (el interno, el segundo: longStream) no se ve interrumpido po un nuevo ciclo largo
-  it('combine stream with switch map', () => {
+  // de cada iteración solo se resolverán si el ciclo corto (el interno, el segundo: longStream) no se ve interrumpido po un nuevo ciclo largo
+  it('combine stream with switchMap', () => {
     longStream.pipe(
       switchMap((long: number) => shortStream.pipe(map((short: number) => `${long} : ${short}`)))
     ).subscribe((resultMap: string) => {
@@ -47,6 +52,17 @@ describe('test timeouts with numeric streams', () => {
     expect(testCallbacks.subscribeCounter).toHaveBeenCalledTimes(7); // iterationStreams * 2
     expect(testResultLastStream.trim()).toEqual(switchMapResultCounter.trim());
   });
+
+  // se resolverán todos los observables, pero respetando los tiempos t el orden de llegada al resolver las iteraciones de cada observable.
+  it('combine stream with concatMap', () => {
+    longStream.pipe(
+      concatMap((long: number) => shortStream.pipe(map((short: number) => `${long} : ${short}`)))
+    ).subscribe((resultMap: string) => {
+      testResultLastStream += resultMap;
+      testCallbacks.subscribeCounter();
+    });
+    clock().tick(MessageTimeout);
+    expect(testCallbacks.subscribeCounter).toHaveBeenCalledTimes(16); // iterationStreams * iterationStreams
+    expect(testResultLastStream.trim()).toEqual(concatMapResultCounter.trim());
+  });
 });
-
-
